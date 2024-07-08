eb04ba27cab9dbbc88c81a2df5e93450ec3c8fa1
Surya Sekhar Sharma <sekharsurya369@gmail.com>
2024-07-08 21:07:19 +0530

    backend

--- Backend/index.js
+++ Backend/index.js
@@ -6,8 +6,20 @@ const __dirname = dirname(__filename);
 const app = express();
 const PORT = 3000;
 
+function logger(req, res, next) { 
+    console.log("request method : ", req.method);
+    console.log("request url : ", req.url);
+    next();
+};
+
+app.use(logger);
+
+// app.get("/", (req, res) => {
+//     res.sendFile(__dirname + "/index1.html");
+// });
+
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/index1.html");
+    res.send("Hello");
 });
 app.listen(PORT, () => {
     console.log(`Server Listening at ${PORT}`);
