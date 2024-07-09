7f6c061300dc4bd3b72b2950496f913abb8f6598
Surya Sekhar Sharma <sekharsurya369@gmail.com>
2024-07-09 23:01:22 +0530

    .

---
+++ Frontend/myapp/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
+import Login from './components/Login';
+import Signup from './components/Signup';
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={Signup} />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
