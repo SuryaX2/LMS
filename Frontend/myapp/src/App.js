769c87fdb341109777836c46acc5edc46b6aaf8a
Surya Sekhar Sharma <sekharsurya369@gmail.com>
2024-07-09 23:40:22 +0530

    changes

--- Frontend/myapp/src/App.js
+++ Frontend/myapp/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './components/Home';
+import Home from './Home';
 // import Login from './components/Login';
 // import Signup from './components/Signup';
 
