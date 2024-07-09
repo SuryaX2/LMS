322c33adce66ea73e06c3813662bf2e39bb3658d
Surya Sekhar Sharma <sekharsurya369@gmail.com>
2024-07-09 23:38:31 +0530

    react

--- Frontend/myapp/src/App.js
+++ Frontend/myapp/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 // import Login from './components/Login';
 // import Signup from './components/Signup';
@@ -7,11 +7,11 @@ import Home from './components/Home';
 function App() {
     return (
         <Router>
-            <Switch>
+            <Routes>
                 <Route path="/" exact component={Home} />
                 {/* <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} /> */}
-            </Switch>
+            </Routes>
         </Router>
     );
 }
