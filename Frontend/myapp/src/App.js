65b5ec720923d476a292e5f99e83531431aa95d8
Surya Sekhar Sharma <sekharsurya369@gmail.com>
2024-07-10 23:45:35 +0530

    .

--- Frontend/myapp/src/App.js
+++ Frontend/myapp/src/App.js
@@ -1,25 +1,9 @@
 import './App.css';
+import Home from './Home';
 
 function App() {
   return (
-    <header class="p-3 text-bg-dark">
-    <div class="container">
-      <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-        <img width="9%"src="https://www.library-management.com/uploads/60196c0c6f3a8_logo_.png" alt="Logo"/>
-        <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-          <li><a href="#home" id="home" class="nav-link px-2 text-secondary">Home</a></li>
-          <li><a href="#about" id="about" class="nav-link px-2 text-white">About</a></li>
-        </ul>
-        <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
-          <input type="search" class="form-control form-control-dark text-bg-dark" placeholder="Search..." aria-label="Search"/>
-        </form>
-        <div class="text-end">
-          <button type="button" class="btn btn-outline-light me-2">Login</button>
-          <button type="button" class="btn btn-warning">Sign-up</button>
-        </div>
-      </div>
-    </div>
-  </header>
+    <Home/>
   );
 }
 
