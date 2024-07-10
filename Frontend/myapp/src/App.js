460236e61f045968eceb8fda01bb36f5f915c309
Surya Sekhar Sharma <sekharsurya369@gmail.com>
2024-07-10 23:48:37 +0530

    .

--- Frontend/myapp/src/App.js
+++ Frontend/myapp/src/App.js
@@ -4,7 +4,16 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <Home/>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/register" element={<Signup />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/home" element={<Home />}></Route>
+        <Route path="/logout" element={<Logout />}></Route>
+        <Route path="/Forgot" element={<Forgot />}></Route>
+      </Routes>
+    </BrowserRouter>
+
   );
 }
 
