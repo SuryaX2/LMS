29f4d19039fa2f0dd5fded45008d9a2dc4de8487
Surya Sekhar Sharma <sekharsurya369@gmail.com>
2024-07-10 21:45:53 +0530

    front

--- Frontend/myapp/src/App.js
+++ Frontend/myapp/src/App.js
@@ -1,19 +1,25 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './components/Home.js';
-import Login from './components/Login';
-import Signup from './components/Signup';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" exact component={Home} />
-                <Route path="/login" component={Login} />
-                <Route path="/signup" component={Signup} />
-            </Routes>
-        </Router>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
