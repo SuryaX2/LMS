575a6675173305e7d6f17d28ad829d31e1170b4f
Surya Sekhar Sharma <sekharsurya369@gmail.com>
2024-07-11 11:51:15 +0530

    .

--- Frontend/myapp/src/App.js
+++ Frontend/myapp/src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 import Home from './Home';
+import Carousel from './Carousel.js';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />}></Route>
+        <Route path="/" element={
+        <>
+            <Home />
+            <Carousel/>  
+        </>
+        }></Route>
       </Routes>
     </BrowserRouter>
 
