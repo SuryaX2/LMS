b6c14d8e7a91c066dd85b5473bcd353bc3c43df3
Surya Sekhar Sharma <sekharsurya369@gmail.com>
2024-07-11 14:02:00 +0530

    .

--- Frontend/myapp/src/Footer.js
+++ Frontend/myapp/src/Footer.js
@@ -11,7 +11,7 @@ function Footer() {
                     <a href="/" class="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
                         <svg class="bi" width="30" height="24"><use href="#bootstrap"></use></svg>
                     </a>
-                    <span class="mb-3 mb-md-0 text-body-secondary">© 2024 Company, Inc</span>
+                    <span class="mb-3 mb-md-0 text-body-secondary">© 2024 LMS, Inc</span>
                 </div>
 
                 <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
