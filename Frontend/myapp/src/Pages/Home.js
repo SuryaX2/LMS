1f2b3e01f1561478659e475e589fd6567dcab6aa
Surya Sekhar Sharma <sekharsurya369@gmail.com>
2024-07-12 12:01:00 +0530

    .

--- Frontend/myapp/src/Pages/Home.js
+++ Frontend/myapp/src/Pages/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Navbar from "../Components/Navbar";
-import Navbar from "../Components/Navbar";
+import Carousel from "../Components/Carousel";
 import Navbar from "../Components/Navbar";
 
 function Home() {
