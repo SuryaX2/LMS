145ee980e1c7f831f2270ed2241e066124babc08
Surya Sekhar Sharma <sekharsurya369@gmail.com>
2024-07-12 11:59:16 +0530

    .

---
+++ Frontend/myapp/src/Pages/Home.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Navbar from "../Components/Navbar";
+import './Home.css';
+
+function Home() {
+    return (
+        <header class="p-3 text-bg-dark">
+            <div class="container">
+                <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+                    <img width="9%" src="https://lmsonline.com/wp-content/uploads/2022/01/cropped-LMS_Logo_FullColor_2017.png" alt="Logo" />
+                    <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+                        <li><a href="#home" id="home" class="nav-link px-2 text-secondary">Home</a></li>
+                        <li><a href="#about" id="about" class="nav-link px-2 text-white">About</a></li>
+                    </ul>
+                    <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
+                        <input type="search" class="form-control form-control-dark text-bg-dark" placeholder="Search..." aria-label="Search" />
+                    </form>
+                    <div class="text-end">
+                        <button type="button" class="btn btn-outline-light me-2">Login</button>
+                        <button type="button" class="btn btn-warning">Sign-up</button>
+                    </div>
+                </div>
+            </div>
+        </header>
+    );
+}
+
+export default Home;
