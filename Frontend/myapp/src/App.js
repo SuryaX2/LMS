ca152ea45f9e357e0f2f4600e6d5bfbaf6baab19
Surya Sekhar Sharma <sekharsurya369@gmail.com>
2024-07-12 12:14:41 +0530

    .

--- Frontend/myapp/src/App.js
+++ Frontend/myapp/src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home.js';
+import Signup from './Pages/Signup.js';
+import Login from './Pages/Login.js';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />}></Route>
-        <Route path="/signup" element={<Home />}></Route>
-        <Route path="/login" element={<Home />}></Route>
+        <Route path="/signup" element={<Signup />}></Route>
+        <Route path="/login" element={<Login />}></Route>
       </Routes>
     </BrowserRouter>
 
