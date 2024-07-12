06cbfafd81c8daee4399cf5652e190ff2cfd6864
Surya Sekhar Sharma <sekharsurya369@gmail.com>
2024-07-12 14:31:17 +0530

    .

--- Frontend/myapp/src/Components/Carousel.js
+++ Frontend/myapp/src/Components/Carousel.js
@@ -2,7 +2,7 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import { Link } from 'react-router-dom';
-import './Carousel.css';
+import './Carousel.module.css';
 
 function Carousel() {
     return (
