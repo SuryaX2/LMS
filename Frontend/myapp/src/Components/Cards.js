ce1d1fb2a0840cbe87cf6aa236ccc725e45a6ce3
Surya Sekhar Sharma <sekharsurya369@gmail.com>
2024-07-12 14:35:19 +0530

    .

--- Frontend/myapp/src/Components/Cards.js
+++ Frontend/myapp/src/Components/Cards.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import './Cards.module.css';
+import './Cards.css';
 
 function Cards() {
     return (
--- Frontend/myapp/src/Components/Carousel.js
+++ Frontend/myapp/src/Components/Carousel.js
@@ -2,7 +2,7 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import { Link } from 'react-router-dom';
-import './Carousel.module.css';
+import './Carousel.css';
 
 function Carousel() {
     return (
