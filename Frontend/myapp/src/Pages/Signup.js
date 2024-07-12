b65b82f33800246b78247ede62fa01a4878a2df3
Surya Sekhar Sharma <sekharsurya369@gmail.com>
2024-07-12 13:58:03 +0530

    .

--- Frontend/myapp/src/Pages/Signup.js
+++ Frontend/myapp/src/Pages/Signup.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 // import AuthContext from '../Context/AuthContext';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import './signup.css';
 
 
 const Signup = () => {
