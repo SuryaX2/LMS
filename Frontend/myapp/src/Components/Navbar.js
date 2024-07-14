64829a96e32063aca10441338c8c97d84da0fdae
Surya Sekhar Sharma <sekharsurya369@gmail.com>
2024-07-14 22:19:02 +0530

    .

--- Frontend/myapp/src/Components/Navbar.js
+++ Frontend/myapp/src/Components/Navbar.js
@@ -1,40 +1,45 @@
 // src/components/navbar.js
 import React from 'react';
-import { Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap';
+import { Navbar, Nav, Form, FormControl, Button,Container  } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 
 function MyNavbar() {
     return (
-        <Navbar bg="dark" variant="dark" expand="lg" >
-            <Navbar.Brand href="#">
-                <img
-                    src="https://lmsonline.com/wp-content/uploads/2022/01/cropped-LMS_Logo_FullColor_2017.png"
-                    width="100"
-                    className="d-inline-block align-top"
-                    alt="Logo"
-                />
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="mr-auto">
-                    <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#about">About</Nav.Link>
-                </Nav>
-                <Form className="d-flex">
-                    <FormControl
-                        type="search"
-                        placeholder="Search"
-                        className="mr-2"
-                        aria-label="Search"
+        <Navbar bg="dark" variant="dark" expand="lg">
+            <Container>
+                <Navbar.Brand href="#">
+                    <img
+                        src="https://lmsonline.com/wp-content/uploads/2022/01/cropped-LMS_Logo_FullColor_2017.png"
+                        width="100"
+                        className="d-inline-block align-top"
+                        alt="Logo"
                     />
-                </Form>
-                <LinkContainer to="/login">
-                    <Button variant="outline-light" className="ml-2">Login</Button>
-                </LinkContainer>
-                <LinkContainer to="/signup">
-                    <Button variant="warning" className="ml-2">Sign-up</Button>
-                </LinkContainer>
-            </Navbar.Collapse>
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav" className="justify-content-between">
+                    <Nav className="mr-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#about">About</Nav.Link>
+                    </Nav>
+                    <Form className="d-flex mx-auto search-form">
+                        <FormControl
+                            type="search"
+                            placeholder="Search"
+                            className="mr-2"
+                            aria-label="Search"
+                        />
+                        <Button variant="outline-light">Search</Button>
+                    </Form>
+                    <div className="d-flex">
+                        <LinkContainer to="/login">
+                            <Button variant="outline-light" className="ml-2">Login</Button>
+                        </LinkContainer>
+                        <LinkContainer to="/signup">
+                            <Button variant="warning" className="ml-2">Sign-up</Button>
+                        </LinkContainer>
+                    </div>
+                </Navbar.Collapse>
+            </Container>
         </Navbar>
     );
 }
