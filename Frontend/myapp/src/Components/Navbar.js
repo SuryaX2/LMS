f9203fe9be7d37425090ebc21cada3b260b55d66
Surya Sekhar Sharma <sekharsurya369@gmail.com>
2024-07-14 22:15:35 +0530

    .

--- Frontend/myapp/src/Components/Navbar.js
+++ Frontend/myapp/src/Components/Navbar.js
@@ -5,7 +5,7 @@ import { LinkContainer } from 'react-router-bootstrap';
 
 function MyNavbar() {
     return (
-        <Navbar bg="dark" variant="dark" expand="lg">
+        <Navbar bg="dark" variant="dark" expand="lg" >
             <Navbar.Brand href="#">
                 <img
                     src="https://lmsonline.com/wp-content/uploads/2022/01/cropped-LMS_Logo_FullColor_2017.png"
@@ -20,9 +20,13 @@ function MyNavbar() {
                     <Nav.Link href="#home">Home</Nav.Link>
                     <Nav.Link href="#about">About</Nav.Link>
                 </Nav>
-                <Form inline className="ml-auto">
-                    <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                    <Button variant="outline-light">Search</Button>
+                <Form className="d-flex">
+                    <FormControl
+                        type="search"
+                        placeholder="Search"
+                        className="mr-2"
+                        aria-label="Search"
+                    />
                 </Form>
                 <LinkContainer to="/login">
                     <Button variant="outline-light" className="ml-2">Login</Button>
