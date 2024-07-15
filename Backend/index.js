4c843b93a9f5c743ef4731e7b2c88e18aa7be0c6
Surya Sekhar Sharma <sekharsurya369@gmail.com>
2024-07-15 12:58:31 +0530

    .

--- Backend/index.js
+++ Backend/index.js
@@ -1,13 +1,8 @@
 import express from "express";
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
 
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 const app = express();
 const PORT = 3001;
-import userRoutes from ""
+import userRoutes from "./routes/user.js"
 
 // app.get("/", (req, res) => {
 //     res.send("Hello");
