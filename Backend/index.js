5faaa15c2cde316c6c17f2ce4737d4c711a2e018
Surya Sekhar Sharma <sekharsurya369@gmail.com>
2024-07-15 13:08:33 +0530

    .

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,14 @@
 import express from "express";
+import cors from 'cors';
 import userRoutes from "./routes/user.js"
+import connectToMongo from "./db.js";
 
 const app = express();
 const PORT = 3001;
+
+connectToMongo();
+app.use(express.json());
+app.use(cors());
 
 app.use("/api/auth", userRoutes);
 // app.get("/", (req, res) => {
