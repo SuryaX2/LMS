5b14d3438ca54b7d9e6a22c943c656716e0d6e8d
Surya Sekhar Sharma <sekharsurya369@gmail.com>
2024-07-15 13:21:48 +0530

    .

--- Backend/routes/user.js
+++ Backend/routes/user.js
@@ -6,7 +6,37 @@ import User from '../models/user.js'
 const router = express.Router();
 
 router.post("/signup", async(req, res) => {
-    res.send("hello");
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(500).json({ errors: errors.array() });
+    }
+    try {
+        let user = await User.findOne({ email: req.body.email });
+        if (user) {
+            return res.status(404).json({ error: "user already exits" });
+        }
+        const salt = await b1.genSalt(6);
+        const spass = await b1.hash(req.body.password, salt);
+        console.log(spass);
+        user = await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: spass,
+        });
+        const data = {
+            user: {
+                id: user.id
+            }
+        }
+        const authtoken = jwt.sign(data, jwt_str);
+        res.json(authtoken)
+
+        //res.json(user)
+    } catch (error) {
+        console.error(error.massage);
+        res.status(600).send("Some Error occured");
+    }
+
 });
 
 export default router;
