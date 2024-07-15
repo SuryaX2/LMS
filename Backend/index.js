559b0979969ca529cbe39ff908cf73e6b0b74280
Surya Sekhar Sharma <sekharsurya369@gmail.com>
2024-07-15 12:58:57 +0530

    .

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,8 @@
 import express from "express";
+import userRoutes from "./routes/user.js"
 
 const app = express();
 const PORT = 3001;
-import userRoutes from "./routes/user.js"
 
 // app.get("/", (req, res) => {
 //     res.send("Hello");
