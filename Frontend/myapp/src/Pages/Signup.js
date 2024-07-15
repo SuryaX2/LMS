4bcd26633d12538cea84382df17620bad855d055
Surya Sekhar Sharma <sekharsurya369@gmail.com>
2024-07-15 14:20:57 +0530

    .

--- Frontend/myapp/src/Pages/Signup.js
+++ Frontend/myapp/src/Pages/Signup.js
@@ -53,7 +53,7 @@ const Signup = () => {
                 <h1 className="text-center text-white text-3xl mt-1 mb-2">BROADEN YOUR HORIZON</h1>
                 <h2 className="text-white text-lg mt-2 mb-1 pt-1 border-t border-white">Sign Up Page</h2>
                 {error && <p className="text-red-500">{error}</p>}
-                <div className="mb-0 flex items-center">
+                <div className="mb-0 flex items-center space-x-4">
                     <label className="text-white mb-1 ">Role:</label>
                     <div className="flex items-center space-x-4 ">
                         <label className="text-white flex">
