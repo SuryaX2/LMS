01e42559f03449fea69615ded992a4e4ccc863b2
Surya Sekhar Sharma <sekharsurya369@gmail.com>
2024-07-15 19:39:43 +0530

    .

--- Frontend/myapp/src/Pages/Forgot.js
+++ Frontend/myapp/src/Pages/Forgot.js
@@ -16,7 +16,7 @@ const Forgot = () => {
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+    <div className="min-h-screen flex items-center justify-center bg-gray-100 ">
       <div className="bg-white p-8 rounded shadow-md w-full max-w-md">
         <h2 className="text-2xl font-bold mb-6">Forgot Password</h2>
         <form onSubmit={handleSubmit}>
