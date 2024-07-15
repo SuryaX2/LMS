4b9ec4a86bebb97b4d4e8d1e3e25818d9affbc1a
Surya Sekhar Sharma <sekharsurya369@gmail.com>
2024-07-15 13:16:40 +0530

    .

--- Backend/routes/user.js
+++ Backend/routes/user.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import bcrypt from 'bcryptjs'
 import jwt from 'jsonwebtoken'
-import User from '../models/user'
+import User from '../models/user.js'
 
 const router = express.Router();
 
