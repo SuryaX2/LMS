50e1598efaf9941db7eb8827cc55da643137a2b6
Surya Sekhar Sharma <sekharsurya369@gmail.com>
2024-07-15 11:36:27 +0530

    .

---
+++ Backend/db.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+
+const uri = "mongodb://localhost:27017/signup"
+
+const connectToMongo = async () => {
+    try {
+        await mongoose.connect(uri);
+        console.log("Conected successful");
+    } catch (error) {
+        console.log("Error: " + error);
+    }
+}
+
+export default connectToMongo
