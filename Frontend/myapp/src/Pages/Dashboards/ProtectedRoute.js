fbc476861f1597ed6277aa338eab84b41b5fab73
Surya Sekhar Sharma <sekharsurya369@gmail.com>
2024-07-16 16:57:19 +0530

    .

--- Frontend/myapp/src/Pages/Dashboards/ProtectedRoute.js
+++ Frontend/myapp/src/Pages/Dashboards/ProtectedRoute.js
@@ -2,19 +2,19 @@ import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
 const ProtectedRoute = ({ component: Component, requiredRole, ...rest }) => {
-  const role = localStorage.getItem('role');
-  return (
-    <Route
-      {...rest}
-      render={props =>
-        role === requiredRole ? (
-          <Component {...props} />
-        ) : (
-          <Redirect to="/login" />
-        )
-      }
-    />
-  );
+    const role = localStorage.getItem('role');
+    return (
+        <Route
+            {...rest}
+            render={props =>
+                role === requiredRole ? (
+                    <Component {...props} />
+                ) : (
+                    <Redirect to="/login" />
+                )
+            }
+        />
+    );
 };
 
 export default ProtectedRoute;
