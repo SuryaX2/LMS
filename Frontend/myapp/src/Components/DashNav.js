65b0abbc75a1364312f1434fd7397c03f58091cb
Surya Sekhar Sharma <sekharsurya369@gmail.com>
2024-07-17 11:47:05 +0530

    .

--- Frontend/myapp/src/Components/DashNav.js
+++ Frontend/myapp/src/Components/DashNav.js
@@ -13,7 +13,6 @@ import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import AdbIcon from '@mui/icons-material/Adb';
 
-const pages = ['Products', 'Pricing', 'Blog'];
 const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
 function ResponsiveAppBar() {
