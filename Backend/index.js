81edc4b0d9e1bfcbf5c6be9fa6533489a6b2d91a
Surya Sekhar Sharma <sekharsurya369@gmail.com>
2024-07-17 14:25:42 +0530

    .

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from 'cors';
-import userRoutes from "./routes/user.js"
+import userRoutes from "./routes/user.js";
 import connectToMongo from "./db.js";
 
 const app = express();
@@ -11,6 +11,8 @@ connectToMongo();
 app.use(express.json());
 app.use(cors());
 app.use("/api/auth", userRoutes);
+app.use('/api/books', bookRouter);
+
 
 app.listen(PORT, () => {
     console.log(`Server Listening at ${PORT}`);
