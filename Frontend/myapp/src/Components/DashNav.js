a889d19e6e91670c7a627dbb4a4424bfc5dc769a
Surya Sekhar Sharma <sekharsurya369@gmail.com>
2024-07-17 13:19:20 +0530

    .

--- Frontend/myapp/src/Components/DashNav.js
+++ Frontend/myapp/src/Components/DashNav.js
@@ -33,7 +33,7 @@ const AdminDashboard = () => {
                         </button>
                         {dropdownOpen && (
                             <div className="absolute right-0 mt-2 w-48 bg-white border rounded shadow-lg">
-                                <button onClick={() => handleNavigation('/dashboard')} className="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-200">Dashboard</button>
+                                <button onClick={() => handleNavigation('/admin-dashboard')} className="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-200">Dashboard</button>
                                 <button onClick={() => handleNavigation('/login')} className="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-200">Logout</button>
                             </div>
                         )}
