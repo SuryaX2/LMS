6375d2c0e1a47733fa65bf8038e881f25deb50fb
Surya Sekhar Sharma <sekharsurya369@gmail.com>
2024-07-17 11:58:20 +0530

    .

--- Frontend/myapp/src/Components/DashNav.js
+++ Frontend/myapp/src/Components/DashNav.js
@@ -90,24 +90,7 @@ function ResponsiveAppBar() {
                         </Menu>
                     </Box>
                     <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
-                    <Typography
-                        variant="h5"
-                        noWrap
-                        component="a"
-                        href="#app-bar-with-responsive-menu"
-                        sx={{
-                            mr: 2,
-                            display: { xs: 'flex', md: 'none' },
-                            flexGrow: 1,
-                            fontFamily: 'monospace',
-                            fontWeight: 700,
-                            letterSpacing: '.3rem',
-                            color: 'inherit',
-                            textDecoration: 'none',
-                        }}
-                    >
-                        LOGO
-                    </Typography>
+                    
 
                     <Box sx={{ flexGrow: 0 }}>
                         <Tooltip title="Open settings">
