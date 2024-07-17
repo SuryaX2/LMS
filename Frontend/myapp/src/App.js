13736872e14a8a1039f1d1bce3586f9083b10b98
Surya Sekhar Sharma <sekharsurya369@gmail.com>
2024-07-17 12:43:30 +0530

    .

--- Frontend/myapp/src/App.js
+++ Frontend/myapp/src/App.js
@@ -22,8 +22,8 @@ function App() {
         </Route>
         <Route element={<ProtectedRoute requiredRole="admin" />}>
           <Route path="/admin-dashboard" element={<AdminDashboard />} />
-        </Route>
         <Route path="/add-book" component={<AddBook/>} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
