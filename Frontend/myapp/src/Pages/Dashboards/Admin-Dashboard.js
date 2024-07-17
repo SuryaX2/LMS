f5cfcb03f107d5d98e27353520c76e15747cd8d5
Surya Sekhar Sharma <sekharsurya369@gmail.com>
2024-07-17 13:45:31 +0530

    .

--- Frontend/myapp/src/Pages/Dashboards/Admin-Dashboard.js
+++ Frontend/myapp/src/Pages/Dashboards/Admin-Dashboard.js
@@ -24,7 +24,7 @@ const AdminDashboard = () => {
 
   return (
     <div className="min-h-screen bg-gray-100 w-screen">
-      <div className="w-full max-w-6xl p-6 bg-white rounded shadow-lg">
+      <div className="w-full max-w-6xl p-1 bg-white rounded shadow-lg">
         <div className="flex justify-between items-center mb-4">
           <h1 className="text-2xl font-bold">Admin Dashboard</h1>
         <div className="mt-4">
