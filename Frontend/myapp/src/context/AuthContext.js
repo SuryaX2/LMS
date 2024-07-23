a5122807cf8a3e1e2c1bd1e4aa757bb2273e102d
Surya Sekhar Sharma <sekharsurya369@gmail.com>
2024-07-23 15:12:32 +0530

    Auth

--- Frontend/myapp/src/context/AuthContext.js
+++ Frontend/myapp/src/context/AuthContext.js
@@ -1,25 +1,17 @@
+// src/context/AuthContext.js
 import React, { createContext, useState } from 'react';
-
 const AuthContext = createContext();
-export { AuthContext }
+export {AuthContext}
 
 export const AuthProvider = ({ children }) => {
-    const [isAuthenticated, setIsAuthenticated] = useState(false);
-    const [user, setUser] = useState(null);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-    const login = (userData) => {
-        setIsAuthenticated(true);
-        setUser(userData);
-    };
-    
-    const logout = () => {
-        setIsAuthenticated(false);
-        setUser(null);
-    };
+  const login = () => setIsAuthenticated(true);
+  const logout = () => setIsAuthenticated(false);
 
-    return (
-        <AuthContext.Provider value={{ isAuthenticated, user, login, logout }}>
-            {children}
-        </AuthContext.Provider>
-    );
+  return (
+    <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
