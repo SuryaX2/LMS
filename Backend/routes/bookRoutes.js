f3eb9a2094c0729498af331019ca9e2e2471b0ce
Surya Sekhar Sharma <sekharsurya369@gmail.com>
2024-07-23 15:13:48 +0530

    backend changes

--- Backend/routes/bookRoutes.js
+++ Backend/routes/bookRoutes.js
@@ -1,11 +1,11 @@
-import express from "express"
-const router = express.Router();
-import bookController from '../controllers/bookController.js';
-import authMiddleware from '../middleware/authMiddleware.js';
+// import express from "express"
+// const router = express.Router();
+// import bookController from '../controllers/bookController.js';
+// import authMiddleware from '../middleware/authMiddleware.js';
 
-router.get('/books', authMiddleware, bookController.getBooks);
-router.get('/user/books', authMiddleware, bookController.getUserBooks);
-router.post('/books/borrow', authMiddleware, bookController.borrowBook);
-router.post('/books/return', authMiddleware, bookController.returnBook);
+// router.get('/books', authMiddleware, bookController.getBooks);
+// router.get('/user/books', authMiddleware, bookController.getUserBooks);
+// router.post('/books/borrow', authMiddleware, bookController.borrowBook);
+// router.post('/books/return', authMiddleware, bookController.returnBook);
 
-export default router;
+// export default router;
