d5fc0fa03fb6f4c6be0893e23ac3e2a33f4fd710
Surya Sekhar Sharma <sekharsurya369@gmail.com>
2024-07-24 13:46:48 +0530

    Database Update

--- Backend/models/Books.js
+++ Backend/models/Books.js
@@ -2,16 +2,39 @@ import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
 const BookSchema = new Schema({
-    title: { type: String, required: true },
-    author: { type: String, required: true },
-    isbn: { type: String, required: true },
-    price: { type: Number, required: true },
-    quantity: { type: Number, required: true },
-    borrowedBy: [{
-        user: { type: Schema.Types.ObjectId, ref: 'User',default: null },
-        borrowedDate: { type: Date, default: Date.now },
-        returnDate: { type: Date, default: null }
-    }]
+    title: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    },
+    isbn: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    quantity: {
+        type: Number,
+        required: true
+    },
+    borrowedBy: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        default: null
+    },
+    borrowedDate: {
+        type: Date,
+        default: null
+    },
+    returnDate: {
+        type: Date,
+        default: null
+    }
 });
 
 const Book = mongoose.model('Book', BookSchema);
