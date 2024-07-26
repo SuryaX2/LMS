c3f447f9dc780517514f8057b404e09957c502ca
Surya Sekhar Sharma <sekharsurya369@gmail.com>
2024-07-26 11:46:21 +0530

    cloudinary setup

---
+++ Backend/utils/cloudinary.js
@@ -0,0 +1,31 @@
+import { v2 as cloudinary } from "cloudinary"
+import fs from "fs"
+
+
+cloudinary.config({
+    cloud_name: 'debpyy8ki',
+    api_key: '661778477594287',
+    api_secret: 'cloudSecret'
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) return null
+        //upload the file on cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        })
+        // file has been uploaded successfull
+        //console.log("file is uploaded on cloudinary ", response.url);
+        fs.unlinkSync(localFilePath)
+        return response;
+
+    } catch (error) {
+        fs.unlinkSync(localFilePath) // remove the locally saved temporary file as the upload operation got failed
+        return null;
+    }
+}
+
+
+
+export { uploadOnCloudinary }
