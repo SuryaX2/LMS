9b535f8503ab7403eef9c584ed0244f81d17f86f
Surya Sekhar Sharma <sekharsurya369@gmail.com>
2024-08-01 21:27:17 +0530

    .

--- Frontend/myapp/src/Pages/Dashboards/ProtectedRoute.js
+++ Frontend/myapp/src/Pages/Dashboards/ProtectedRoute.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
-
 const ProtectedRoute = ({ requiredRole }) => {
-  const role = localStorage.getItem('role');
+const role = localStorage.getItem('role');
 
   if (role === requiredRole) {
     return <Outlet />;
