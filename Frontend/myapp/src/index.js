6503ffc27c61df7c7ab485c00db330e364ab43e7
Surya Sekhar Sharma <sekharsurya369@gmail.com>
2024-08-12 18:29:43 +0530

    .

--- Frontend/myapp/src/index.js
+++ Frontend/myapp/src/index.js
@@ -8,9 +8,11 @@ import { AuthProvider } from './context/AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <React.StrictMode>
     <AuthProvider>
       <App />
     </AuthProvider>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
