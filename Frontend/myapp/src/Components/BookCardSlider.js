87256424be74ebf746bd460e34eed17eec3f77d5
Surya Sekhar Sharma <sekharsurya369@gmail.com>
2024-08-23 22:26:40 +0530

    .

--- Frontend/myapp/src/Components/BookCardSlider.js
+++ Frontend/myapp/src/Components/BookCardSlider.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Pagination } from 'swiper/modules';
+import { Navigation, Pagination, Autoplay } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
+import 'swiper/css/autoplay';
 
 const BookCardSlider = () => {
   const [books, setBooks] = useState([]);
@@ -22,11 +23,16 @@ const BookCardSlider = () => {
   return (
     <div className="book-card-slider container mx-auto my-8">
       <Swiper
-        modules={[Navigation, Pagination]}
+        modules={[Navigation, Pagination, Autoplay]}
         spaceBetween={20}
         slidesPerView={1}
         navigation
         pagination={{ clickable: true }}
+        autoplay={{
+          delay: 2000,
+          disableOnInteraction: false,
+        }}
+        loop={true}
         breakpoints={{
           640: {
             slidesPerView: 2,
@@ -41,9 +47,9 @@ const BookCardSlider = () => {
       >
         {books.filter((book) => book.quantity > 0).map((book) => (
           <SwiperSlide key={book._id}>
-            <div className="bg-black-500 shadow-lg shadow-white cursor-pointer rounded-lg overflow-hidden h-full">
+            <div className="bg-black-500 shadow-lg shadow-white cursor-pointer rounded-lg overflow-hidden h-full mb-5">
               <img 
-                className="w-full h-64 object-cover object-top opacity-100"
+                className="w-full h-96 object-cover object-top opacity-100"
                 src={book.avatar} 
                 alt={book.title} 
               />
