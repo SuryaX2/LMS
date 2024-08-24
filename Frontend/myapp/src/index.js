64be0676503a43e0c60d1a001a245941197c6022
Surya Sekhar Sharma <sekharsurya369@gmail.com>
2024-08-24 14:00:29 +0530

    .

--- Frontend/myapp/src/index.js
+++ Frontend/myapp/src/index.js
@@ -4,14 +4,11 @@ import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import reportWebVitals from './reportWebVitals';
-// import { AuthProvider } from './context/AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <AuthProvider> */}
       <App />
-    {/* </AuthProvider> */}
   </React.StrictMode>
 );
 
