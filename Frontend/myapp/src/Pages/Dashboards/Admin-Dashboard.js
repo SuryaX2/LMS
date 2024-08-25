921b19605c6ce6ec90dc0b037bff5ee85e9d725e
Surya Sekhar Sharma <sekharsurya369@gmail.com>
2024-08-25 12:31:11 +0530

    .

--- Frontend/myapp/src/Pages/Dashboards/Admin-Dashboard.js
+++ Frontend/myapp/src/Pages/Dashboards/Admin-Dashboard.js
@@ -217,10 +217,11 @@ const AdminDashboard = () => {
             {/* book inventory div */}
             <div className="mb-8">
               <div className="flex items-center justify-between">
-                <h2 className="text-3xl font-bold text-gray-800 relative">
+                <h2 className="text-3xl font-bold text-white relative">
                   Book Inventory
                   <span className="absolute bottom-0 left-0 w-1/3 h-1 bg-blue-500 rounded-full"></span>
                 </h2>
+                
                 <div className="relative w-64"> {/* Wrapper for search input with fixed width */}
                   <FormControl
                     type="search"
@@ -231,7 +232,7 @@ const AdminDashboard = () => {
                   />
                 </div>
               </div>
-              <p className="text-gray-600 mt-2">Manage your library's collection with ease</p>
+              <p className="text-gray-300 mt-2">Manage your library's collection with ease</p>
             </div>
             {/* book card div */}
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
