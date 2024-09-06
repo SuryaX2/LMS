ef0562117bb588edce6e8ebd77eabd4d9a06b840
Surya Sekhar Sharma <sekharsurya369@gmail.com>
2024-09-06 11:45:38 +0530

    .

--- Frontend/myapp/src/Components/Navbar.js
+++ Frontend/myapp/src/Components/Navbar.js
@@ -4,28 +4,28 @@ import { LinkContainer } from 'react-router-bootstrap';
 
 function MyNavbar() {
     return (
-        <Navbar bg="dark" variant="dark" expand="lg" className='p-3'>
-            <Container>
-                <Navbar.Brand href="#">
+        <Navbar bg="dark" variant="dark" expand="lg" className='py-2'>
+            <Container fluid>
+                <Navbar.Brand href="#" className="me-auto">
                     <img
                         src="https://lmsonline.com/wp-content/uploads/2022/01/cropped-LMS_Logo_FullColor_2017.png"
-                        width="100"
+                        width="80"
                         className="d-inline-block align-top"
                         alt="Logo"
                     />
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav" className="justify-content-between">
-                    <Nav className="mr-auto">
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto mb-2 mb-lg-0">
                         <Nav.Link href="#home">Home</Nav.Link>
                         <Nav.Link href="#about">About</Nav.Link>
                     </Nav>
-                    <div className="d-flex">
+                    <div className="d-flex flex-column flex-lg-row gap-2">
                         <LinkContainer to="/login">
-                            <Button variant="outline-light" className="ml-2">Login</Button>
+                            <Button variant="outline-light" className="w-100">Login</Button>
                         </LinkContainer>
                         <LinkContainer to="/signup">
-                            <Button variant="warning" className="ml-2">Sign-up</Button>
+                            <Button variant="warning" className="w-100">Sign-up</Button>
                         </LinkContainer>
                     </div>
                 </Navbar.Collapse>
